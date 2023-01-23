86e58345293865581c509fa6a9892278cb45678d
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-23 10:31:23 +0100

    Starting point front-end
    Starting point front-end

---
+++ src/redux/actions/index.js
@@ -0,0 +1,58 @@
+export const getUserProfile = () => {
+  return async (dispatch, getState) => {
+    const options = {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdmNWNmYWQ4MzkzNTAwMTVlOGM0YTUiLCJpYXQiOjE2NzQ0NjUxMjgsImV4cCI6MTY3NTY3NDcyOH0.w0jJ0XgMxZeslQivku-2ce45Dx9rwGNzfuz2_fesLS8",
+      },
+    };
+
+    const res = await fetch(
+      "https://striveschool-api.herokuapp.com/api/profile/me",
+      options
+    );
+    const userData = await res.json();
+    dispatch({
+      type: "GET_USER_PROFILE",
+      payload: userData,
+    });
+  };
+};
+export const newTextArea = (userName) => {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: "GET_NEW_NAME",
+      payload: userName,
+    });
+  };
+  console.log(userName);
+};
+
+export const submitEditProfile = (e, name, surname, title, bio, area) => {
+  e.preventDefault();
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdmNWNmYWQ4MzkzNTAwMTVlOGM0YTUiLCJpYXQiOjE2NzQ0NjUxMjgsImV4cCI6MTY3NTY3NDcyOH0.w0jJ0XgMxZeslQivku-2ce45Dx9rwGNzfuz2_fesLS8",
+    },
+    body: JSON.stringify({
+      name: name,
+      surname: surname,
+      title: title,
+      bio: bio,
+      area: area,
+    }),
+  };
+  return async (dispatch, getState) => {
+    const res = await fetch(
+      "https://striveschool-api.herokuapp.com/api/profile/",
+      options
+    );
+    console.log(await res.json());
+
+    dispatch(getUserProfile());
+  };
+};
