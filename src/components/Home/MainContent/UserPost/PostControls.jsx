4935a26eff93cdbfe1c264cd62bf42f6009ae96e
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-27 11:47:52 +0100

    update llike

--- src/components/Home/MainContent/UserPost/PostControls.jsx
+++ src/components/Home/MainContent/UserPost/PostControls.jsx
@@ -4,14 +4,28 @@ import { FaThumbsUp, FaRegComment, FaShareAlt } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useStore } from "react-redux";
+import { useSelector } from "react-redux";
 export const PostControls = ({ post }) => {
+  const { user: currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
   const [like, setLike] = useState(false);
+  const [myBody, setBody] = useState({
+    username: currentUser._id,
+    post: post._id,
+  });
+  useEffect(() => {}, [currentUser]);
 
   const fetchLike = async (query) => {
     console.log(query);
     const options = {
       method: query === true ? "POST" : "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myBody),
     };
+    console.log(myBody);
     const response = await fetch(
       `https://hilarious-toothbrush-production.up.railway.app/posts/${post._id}/like`,
       options
@@ -20,8 +34,21 @@ export const PostControls = ({ post }) => {
     const data = await response.json();
     return data;
   };
+  const checkIfItIsLike = async () => {
+    const response = await fetch(
+      `https://hilarious-toothbrush-production.up.railway.app/posts/${post._id}`
+    );
+    const data = await response.json();
+    if (data.likes.includes(currentUser._id)) {
+      console.log(like);
+      setLike(true);
+    } else {
+      setLike(false);
+    }
+  };
 
-  const handleLike = () => {
+  const handleLike = async () => {
+    await checkIfItIsLike();
     console.log("handleLike triggered");
     if (like) {
       setLike(false);
