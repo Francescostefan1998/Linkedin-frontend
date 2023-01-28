0b2e7c39384f6566e6f3cd6f98edb74cd3670d3f
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-28 10:21:45 +0100

    styling the comment list
    styling the comment list

--- src/components/Home/MainContent/UserPost/AddAComment.jsx
+++ src/components/Home/MainContent/UserPost/AddAComment.jsx
@@ -7,6 +7,10 @@ import {
   FaRegSmile,
 } from "react-icons/fa";
 import { SlPicture } from "react-icons/sl";
+import { MdArrowDropDown } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
+import { BsDot } from "react-icons/bs";
+import { GrLike } from "react-icons/gr";
 
 import { FiSend } from "react-icons/fi";
 import { useState } from "react";
@@ -79,17 +83,75 @@ export const AddAComment = ({ post }) => {
         </div>
       </div>
       <div>
+        <div className="AddAcomment-mostRelevant">
+          Most relevant
+          <MdArrowDropDown className="addComment-arrow-icon" />
+        </div>
         {postComments.length > 0 ? (
           <div>
             {postComments.map((comment, index) => (
-              <div className="addAcomment-commentList">
-                {index + 1}, {comment.comment}
+              <div className="addAcomment-singleComment">
+                <div className="addAcomment-singleComment-picture">image</div>
+                <div className="addAcomment-singleComment-main">
+                  <div className="addAcomment-commentList">
+                    <div className="addAcomment-commentList-top">
+                      <div className="sectionOnTheTop">
+                        <div>
+                          <p>
+                            <strong>name</strong>
+                          </p>
+                        </div>
+                        <div>
+                          <p className="gray">other sharing</p>
+                        </div>
+                      </div>
+                      <div className="sectionOnTheTop baseLine">
+                        <div>
+                          <p className="gray">name</p>
+                        </div>
+                        <div>
+                          <p className="gray">
+                            <BsThreeDots className="dots" />
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="twodash">--</div>
+
+                    <div className="big-space">
+                      {index + 1}, {comment.comment}
+                    </div>
+                  </div>
+                  <div className="under-message">
+                    <div className="message-flex">
+                      <div>advice</div>
+                      <div>
+                        <BsDot className="dot" />
+                      </div>
+                      <div>
+                        <GrLike className="like" />
+                      </div>
+                      <div>2</div>
+                    </div>
+                    <div className="message-flex-verticla-line"></div>
+                    <div className="message-flex">
+                      <div>answer</div>
+                      <div>
+                        <BsDot className="dot" />
+                      </div>
+                      <div>answer</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
         ) : (
           <div></div>
         )}
+        <div className="AddAcomment-mostRelevant loads">
+          Load other comments
+        </div>
       </div>
     </div>
   );
